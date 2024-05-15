98f7d088ececc7747e1e9557bba04ff8c644535a
Hazem Ya-sin <hazem.yasin.1416@gmail.com>
2024-05-15 05:32:25 +0300

    got the source code

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,6 +1,16 @@
+using GameStore.Api.Data;
+using GameStore.Api.Endpoints;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connString = builder.Configuration.GetConnectionString("GameStore");
+builder.Services.AddSqlite<GameStoreContext>(connString);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGamesEndpoints();
+app.MapGenresEndpoints();
+
+await app.MigrateDbAsync();
 
 app.Run();
